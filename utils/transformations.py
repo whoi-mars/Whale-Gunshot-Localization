b6790ff1ebdd626cf8a4856265a6b68c04e6d1fd
Mark Goldwater <goldwater243@gmail.com>
2023-02-28 18:26:34 +0000

    Added normalize1dchannel class and get_transforms function

--- utils/transformations.py
+++ utils/transformations.py
@@ -2,6 +2,9 @@ from scipy.signal import stft
 import yaml
 import numpy as np
 
+import torch
+from torchvision import transforms
+
 # load config file
 with open("config.yaml", 'r') as yaml_file:
     config = yaml.load(yaml_file, Loader=yaml.Loader)
@@ -39,3 +42,59 @@ def to_spect(x):
 
     # flip about time axis, and add channel dimension
     return np.expand_dims(np.flip(log_spect, axis=channel_dim), axis=channel_dim)
+
+class Normalize1DChannel:
+    """
+    Normalizes spectrograms row-wise by mean-centering and dividing by std
+
+    ...
+
+    Attributes
+    ----------
+    mu_list : array-like
+        list of row-wise means for each spectrogram
+    std_list : array-like
+        list of row-wise stds for each spectrogram
+    num_signals : int
+        number of input spectrograms
+    """
+
+    def __init__(self, mu_list, std_list):
+        """
+        Construct attributes.
+
+        Parameters
+        ----------
+        mu_list : array-like
+            list of row-wise means for each spectrogram
+        std_list : array-like
+            list of row-wise stds for each spectrogram
+        """
+
+        self.mu_list = torch.as_tensor(mu_list).float()
+        self.std_list = torch.as_tensor(std_list).float()
+        self.num_signals = mu_list.shape[0]
+
+    def norm(self, x):
+        """divide input spectrogram rows by provided mean and divide by provided std"""
+
+        return (x - self.mu_list.view(self.num_signals, 1, -1, 1)) / (self.std_list.view(self.num_signals, 1, -1, 1))
+
+    def __call__(self, tensor):
+        return self.norm(tensor)
+
+def get_image_transform():
+
+    # load mean and std
+    mu_list = np.load(config['dataset']['data_directory'] + '/mean.npy', allow_pickle=True)
+    std_list = np.load(config['dataset']['data_directory'] + '/std.npy', allow_pickle=True)
+    
+    transform_eval = transforms.Compose([
+        Normalize1DChannel(mu_list, std_list)
+    ])
+
+    transform_train = transforms.Compose([
+        Normalize1DChannel(mu_list, std_list)
+    ])
+
+    return {'train' : transform_train, 'eval' : transform_eval}
