ed7b5d3271596572c8e83334ca7b249ccb030ec1
Mark Goldwater <goldwater243@gmail.com>
2023-03-15 19:11:22 +0000

    Added frequency band zeroing class

--- utils/transformations.py
+++ utils/transformations.py
@@ -86,6 +86,90 @@ class Normalize1DChannel:
     def __call__(self, tensor):
         return self.norm(tensor)
 
+class FrequencyBandZeroing:
+
+    """
+    Class to be used in transforms.Compose() to randomly zero out frequencies.
+
+    ...
+
+    Attributes
+    ----------
+    max_freq_width : int
+        maximum width of continuous frequency band to zero out
+    max_t_width : int
+        maximum width of continuous time band to zero out
+    num_f : int
+        how many continuous frequency bands to zero out
+    num_t : int 
+        how many continuous time bands to zero out
+    """
+
+    def  __init__(self, max_freq_width = 30, max_t_width=30, num_f=2, num_t=4):
+        """
+        Construct attributes
+
+        Parameters
+        ----------
+        max_freq_width : int
+            maximum width of continuous frequency band to zero out
+        max_t_width : int
+            maximum width of continuous time band to zero out
+        num_f : int
+            how many continuous frequency bands to zero out
+        num_t : int 
+            how many continuous time bands to zero out
+        """
+        
+        self.max_freq_width = max_freq_width
+        self.max_t_width = max_t_width
+        self.num_f = num_f
+        self.num_t = num_t
+
+    def freq_band_zeroing(self, x):
+
+        """
+        Function to randomly zero-out a band of frequencies and/or times
+
+        Parameters
+        ----------
+        x : array-like 
+            input spectrogram
+        max_freq_width : int
+            maximum continuous bandwidth to zero
+
+        Returns
+        -------
+        x : array-like
+            spectrogram with zeroed-out frequencies
+        """
+
+        if len(x.shape) == 3:
+            x = x.unsqueeze(1)
+            N, _, H, W = x.shape
+        elif len(x.shape) == 4:
+            N, _, H, W = x.shape
+        else:
+            raise ValueError(f"x must be shape (N, H, W) or (N, C, H, W), is now {len(x.shape)}")
+        
+        zero_width_f = torch.randint(0, self.max_freq_width, size=(self.num_f,))
+        offset_f = torch.randint(0, H - zero_width_f.max(), size=(self.num_f,))
+
+        for wf, of in zip(zero_width_f, offset_f):
+            x[:,:,of:(of + wf),:] = 0
+
+        if self.num_t:
+            zero_width_t = torch.randint(0, self.max_t_width, size=(self.num_t,))
+            offset_t = torch.randint(0, W - zero_width_t.max(), size=(self.num_t,))
+            
+            for wf, of in zip(zero_width_t, offset_t):
+                x[:,:,:,of:(of + wf)] = 0
+
+        return x
+
+    def __call__(self, tensor):
+        return self.freq_band_zeroing(tensor)
+
 def get_image_transform():
     """
     Gets dictionary of spectrogram preprocessing transforms for training and evaluation.
