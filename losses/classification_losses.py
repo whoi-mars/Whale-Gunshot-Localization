36bf6e9f9f0572fdd390468f9f6fa6932dee03a9
Mark Goldwater <goldwater243@gmail.com>
2023-04-18 02:48:36 +0000

    Fixed classification loss

--- losses/classification_losses.py
+++ losses/classification_losses.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
 
-class SigBCE:
+class SigBCE(nn.Module):
     """
     Wrapper for BCE loss and sigmoid activation function
     """
     def __init__(self):
+        super(SigBCE, self).__init__()
         self.sigmoid = nn.Sigmoid()
         self.BCE = nn.BCELoss()
     
