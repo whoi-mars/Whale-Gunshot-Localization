f31a055721be4fd2f3bd83d0b526ba2ff7c3c939
Mark Goldwater <goldwater243@gmail.com>
2023-06-02 01:18:10 +0000

    Script to scan a single file

--- scripts/scan_file.py
+++ scripts/scan_file.py
@@ -1,5 +1,7 @@
 import os
 
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 import librosa
 import numpy as np
 import pandas as pd
@@ -9,10 +11,10 @@ import torch
 
 from whale_gunshot_localization.models.tcn_archs import TCNRangeAndClassify
 from whale_gunshot_localization.utils.experimental import ClipAnalyzer
-from whale_gunshot_localization import config
+from whale_gunshot_localization import config, PROJECT_ROOT_DIR
 
 # config
-FILE = os.path.join(config['dataset']['ccb_data_directory'], '6468/6468.220329121454.wav')
+FILE = os.path.join(config['dataset']['ccb_data_directory'], '6470/6470.220406001610.wav')
 OVERLAP_FRACTION = 0.75 # in (0, 1)
 CHUNK_SIZE = 160 # [s]
 
@@ -26,9 +28,13 @@ size = fs*T
 # get device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 if torch.cuda.is_available():
-    print("Using the GPU!")
+    print("Using the GPU!\n")
 else:
-    print("WARNING: Could not find GPU. Using CPU only.")
+    print("WARNING: Could not find GPU. Using CPU only.\n")
+
+# make results CSV file if it doesn't exist
+if not os.path.exists(os.path.join(PROJECT_ROOT_DIR, 'scan_results.csv')):
+    pd.DataFrame(columns=["file_name", "timestamp"]).to_csv(os.path.join(PROJECT_ROOT_DIR, 'scan_results.csv'), index=False)
 
 # load model
 model = TCNRangeAndClassify(input_size=232,
@@ -46,16 +52,25 @@ file_duration = librosa.get_duration(filename=FILE)
 file_samplerate = librosa.get_samplerate(path=FILE)
 
 pointer = 0
-while file_duration - pointer > 0:
+c = 0
+with tqdm(total=file_duration // CHUNK_SIZE) as pbar:
+    while file_duration - pointer > CHUNK_SIZE:
 
-    # get CHUNK of data and downsample
-    y, _ = librosa.load(FILE, sr=file_samplerate, offset=pointer, duration=CHUNK_SIZE)
-    y = signal.resample_poly(y, fs, file_samplerate)
-    
-    # scan CHUNK
-    _, range_measurements, timestamps = CA.process_clips(y)[0]
+        # get CHUNK of data and downsample
+        y, _ = librosa.load(FILE, sr=file_samplerate, offset=pointer, duration=CHUNK_SIZE)
+        y = signal.resample_poly(y, fs, file_samplerate)
+        
+        # scan CHUNK
+        imgs, range_measurements, timestamps = CA.process_clips(y)[0]
 
-    if len(timestamps):
-        pass
-    break
-    pointer += CHUNK_SIZE
+        if len(timestamps):
+            new_row = pd.DataFrame({'file_name' : [FILE for _ in timestamps], 'timestamp' : [pointer + t for t in timestamps]})
+            new_row.to_csv('scan_results.csv', mode='a', index=False, header=False)
+            
+            for i in imgs:
+                plt.imshow(i)
+                plt.savefig(f'{c}.png')
+                c += 1
+
+        pointer += CHUNK_SIZE
+        pbar.update(1)
--- whale_gunshot_localization/__init__.py
+++ whale_gunshot_localization/__init__.py
@@ -4,6 +4,9 @@ import yaml
 # get package root directory
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 
+# get project root directory
+PROJECT_ROOT_DIR = os.path.dirname(ROOT_DIR)
+
 # load config file
 with open(os.path.join(ROOT_DIR, "config.yaml"), 'r') as yaml_file:
     config = yaml.load(yaml_file, Loader=yaml.Loader)
