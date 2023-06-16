0de31e2266ad96c424f2fae1b92fc91a81076600
Mark Goldwater <goldwater243@gmail.com>
2023-06-16 14:16:20 +0000

    script to count detections in 2 minute bins

---
+++ scripts/count_detections.py
@@ -0,0 +1,120 @@
+import os
+import glob
+import argparse
+
+import torch
+from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
+
+from whale_gunshot_localization.models.tcn_archs import TCNRangeAndClassify
+from whale_gunshot_localization.utils.experimental import ClipAnalyzer, l2_standardize
+from whale_gunshot_localization import config, PROJECT_ROOT_DIR
+from scan_file import scan_file
+
+parser = argparse.ArgumentParser(description="Scan all files in sensor.")
+parser.add_argument('--scan', action='store_true',
+                    help='whether to scan before plotting')
+parser.add_argument('--sensor', '-s', type=int,
+                    help='which sensor to scan')
+parser.add_argument('-o', '--overlap_fraction', type=float, default=0.75, metavar="[float in (0, 1)]", required=False,
+                    help='how much window to overlap when scanning the file (default: 0.75)')
+parser.add_argument('-c', '--chunk_size', type=float, default=160, required=False,
+                    help='chunk of WAV file to process simultaneously with the TCN in seconds (default: 160)')
+args = parser.parse_args()
+
+def sort_wav_chronological(wav_files, xml_files):
+
+    start_times = []
+
+    for wf in wav_files:
+
+        # get corresponding xml file
+        xml_file = wf.split('.wav')[0] + ".log.xml"
+
+        # get start time
+        with open(xml_file, 'r') as f:
+            data = f.read()
+        data = BeautifulSoup(data, features='lxml')
+        start_time = np.datetime64(data.find_all("wavfilehandler", samplingstarttimelocal=True)[0]['samplingstarttimelocal'])
+        start_times.append(start_time)
+
+    # get sorted indices
+    idx_sorted = np.argsort(start_times)
+
+    start_times = np.asarray(start_times)
+    
+    return wav_files[idx_sorted], start_times[idx_sorted]
+
+
+if __name__ == "__main__":
+    
+    if args.scan:
+        # get device
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if torch.cuda.is_available():
+            print("Using the GPU!\n")
+        else:
+            print("WARNING: Could not find GPU. Using CPU only.\n")
+
+        # load model
+        model = TCNRangeAndClassify(input_size=232,
+                                    num_channels=7*[368],
+                                    kernel_size=6,
+                                    dropout=0.05).to(device)
+        state_dict = torch.load(os.path.join(config['models']['checkpoints_directories'] + '/range_classify', 'weights_best.pt'), map_location=device)
+        model.load_state_dict(state_dict['model_state_dict'])
+
+        # get files
+        wav_files = np.asarray(glob.glob(os.path.join(config['dataset']['ccb_data_directory'], str(args.sensor), "*.wav")))
+        xml_files = np.asarray(glob.glob(os.path.join(config['dataset']['ccb_data_directory'], str(args.sensor), "*.xml")))
+
+        # sort wav files chronologically
+        wav_files, start_times = sort_wav_chronological(wav_files, xml_files)
+
+        data_params = {'mu_list' : np.load(config['dataset']['data_directory'] + '/mean_range_classification.npy', allow_pickle=True),
+                    'std_list' : np.load(config['dataset']['data_directory'] + '/std_range_classification.npy', allow_pickle=True),
+                    'fs' : config['signal']['fs'],
+                    'T' : config['signal']['T'],}
+
+        # scan file
+        scan_file(file=wav_files[1],
+                    data_params=data_params,
+                    overlap_fraction=args.overlap_fraction, 
+                    chunk_size=args.chunk_size, 
+                    model=model,
+                    device=device)
+    
+    # results path
+    path = os.path.join(PROJECT_ROOT_DIR, "scripts", "results", "scan_results.csv")
+
+    if os.path.exists(path):
+        df = pd.read_csv(path)
+    else:
+        raise RuntimeError(f'{path} does not exist, you must scan the data before plotting')
+
+    # get sensors in results CSV
+    sensors = list(set(df["sensor"]))
+    
+    # make dictionary from file --> start time
+    wav_files, xml_files = [], []
+    for sensor in sensors:
+        wav_files.extend(glob.glob(os.path.join(config['dataset']['ccb_data_directory'], str(sensor), "*.wav")))
+        xml_files.extend(glob.glob(os.path.join(config['dataset']['ccb_data_directory'], str(sensor), "*.xml")))
+    wav_files = np.asarray(wav_files)
+    xml_files = np.asarray(xml_files)
+    wav_files, start_times = sort_wav_chronological(wav_files, xml_files)
+    time_dict = dict(zip(wav_files, start_times))
+
+    # add column with timestamp relative to global time
+    df["global_timestamp"] = df.apply(lambda row : time_dict[row['file_name']] + np.timedelta64(int(row['timestamp'] * 1000), 'ms'), axis=1)
+    
+    bins_dt = pd.date_range(start=df["global_timestamp"].min(), end=df["global_timestamp"].max(), freq="2min")
+    bins_str = bins_dt.astype(str).values
+
+    labels = ['({}, {}]'.format(bins_str[i-1], bins_str[i]) for i in range(1, len(bins_str))]
+
+    # df['cat'] = pd.cut(df['Date'], bins=bins_dt, labels=labels)
+    
+    df['bin'] = pd.cut(df["global_timestamp"], bins=bins_dt, labels=labels)
+    print(df['bin'])
