e944452f95b313fb7e1eb45c6ada963f5828be61
Mark Goldwater <goldwater243@gmail.com>
2023-06-30 15:47:30 +0000

    Switch to 12 TOSSIT

--- whale_gunshot_localization/__init__.py
+++ whale_gunshot_localization/__init__.py
@@ -8,5 +8,5 @@ ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 PROJECT_ROOT_DIR = os.path.dirname(ROOT_DIR)
 
 # load config file
-with open(os.path.join(ROOT_DIR, "config_5.yaml"), 'r') as yaml_file:
+with open(os.path.join(ROOT_DIR, "config_12.yaml"), 'r') as yaml_file:
     config = yaml.load(yaml_file, Loader=yaml.Loader)
