23a738fcf6550125b211889cbbee25610d0d13f8
Mark Goldwater <goldwater243@gmail.com>
2023-07-27 23:12:44 +0000

    Added description

--- scripts/scan_experimental_data.py
+++ scripts/scan_experimental_data.py
@@ -1,3 +1,7 @@
+"""
+Script to scan multiple sensors of experimental data.
+"""
+
 import os
 import glob
 import yaml
@@ -14,6 +18,7 @@ import torch
 import pandas as pd
 from scipy import signal
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 from whale_gunshot_localization.models.tcn_archs import TCNRangeAndClassify
 import whale_gunshot_localization.utils.experimental as experimental
