12a05c28348c76ca7c7c555662c4f054a2e76e0b
Mark Goldwater <goldwater243@gmail.com>
2023-07-27 23:08:50 +0000

    Properly saving for MATLAB and Python

--- scripts/collect_noise.py
+++ scripts/collect_noise.py
@@ -1,3 +1,7 @@
+"""
+Script to collect noise to store in H5 file for training and in a MAT file for simulated data geneation.
+"""
+
 import os
 import argparse
 
@@ -104,15 +108,20 @@ def collect_noise(n, fs_target, T_target):
         
         # downsample and save
         wav = resample_poly(wav, fs_target, f_samplerate)
+        
         X[i,:] = wav
-
-    with h5py.File(os.path.join(config['dataset']['data_directory'], f"{args.save_name}.h5"), "w") as f:
-        f.create_dataset('data', data=X, shape=X.shape, chunks=(1, X.shape[1]))
-        f.create_dataset('fs', data=args.sample_rate, shape=(1,)) 
 
     # save as MAT file
     mdict = {u'noise_from_data': X.T, u'fs': float(fs_target)}
     hdf5storage.savemat(os.path.join(config['dataset']['data_directory'], f"{args.save_name}.mat"), mdict, format="7.3")
+
+    # mean-center and L2 norm for h5 noise
+    X = X - X.mean(axis=1, keepdims=True)
+    X = X / np.sqrt(np.sum(X ** 2, axis=1, keepdims=True))
+
+    with h5py.File(os.path.join(config['dataset']['data_directory'], f"{args.save_name}.h5"), "w") as f:
+        f.create_dataset('data', data=X, shape=X.shape, chunks=(1, X.shape[1]))
+        f.create_dataset('fs', data=args.sample_rate, shape=(1,)) 
 
 if __name__ == "__main__":
 
