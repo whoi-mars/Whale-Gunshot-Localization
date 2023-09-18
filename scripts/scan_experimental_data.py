652b7facbf1e86efe400e28307a5f278318211ce
Mark Goldwater <goldwater243@gmail.com>
2023-09-18 18:21:57 -0400

    Working on plotting tool

--- scripts/scan_experimental_data.py
+++ scripts/scan_experimental_data.py
@@ -27,6 +27,7 @@ import pygmt
 
 from whale_gunshot_localization.models.tcn_archs import TCNRangeAndClassify
 import whale_gunshot_localization.utils.experimental as experimental
+import whale_gunshot_localization.utils.plotting as plotting
 from whale_gunshot_localization import config, PROJECT_ROOT_DIR
 
 # parse input arguments
@@ -54,66 +55,66 @@ if args.scan:
     else:
         print("WARNING: Could not find GPU. Using CPU only.", flush=True)
 
-def plot_localization(locs_est, buffer=6000, title=None, dates=None, save=None, legend_transparency=0): 
-    """
-    Plot estimated source locations.
+# def plot_localization(locs_est, buffer=6000, title=None, dates=None, save=None, legend_transparency=0): 
+#     """
+#     Plot estimated source locations.
 
-    Parameters
-    ----------
-    locs_est : List[np.array], with each subarray of shape N X 2
-        list of lists of estimated locations where the first column stores the Y
-        coordinate and the second stores the X coordinates
-    buffer : float
-        how much to plot outside of the limits established in the config file
-    title : str
-        plot title
-    dates : List[datetime.datetime]
-        list of dates associated with each sublist of location estiamtes
-        in locs_est
-    save : str
-        path at which to save the plot if desired
-    """
+#     Parameters
+#     ----------
+#     locs_est : List[np.array], with each subarray of shape N X 2
+#         list of lists of estimated locations where the first column stores the Y
+#         coordinate and the second stores the X coordinates
+#     buffer : float
+#         how much to plot outside of the limits established in the config file
+#     title : str
+#         plot title
+#     dates : List[datetime.datetime]
+#         list of dates associated with each sublist of location estiamtes
+#         in locs_est
+#     save : str
+#         path at which to save the plot if desired
+#     """
 
-    # format inputs
-    if not isinstance(locs_est, list):
-        locs_est = [locs_est]
-    if not isinstance(dates, list):
-        dates = [dates]
-    assert len(locs_est) == len(dates), "locs_est and dates lists must have a one-to-one correspondence"
+#     # format inputs
+#     if not isinstance(locs_est, list):
+#         locs_est = [locs_est]
+#     if not isinstance(dates, list):
+#         dates = [dates]
+#     assert len(locs_est) == len(dates), "locs_est and dates lists must have a one-to-one correspondence"
 
-    # random list of color for plotting
-    rng = np.random.default_rng(1111)
-    colors = [rng.uniform(0, 255, size=3) for _ in range(len(locs_est))]
+#     # random list of color for plotting
+#     rng = np.random.default_rng(1111)
+#     colors = [rng.uniform(0, 255, size=3) for _ in range(len(locs_est))]
 
-    # load constants
-    TOSSIT_locations = np.asarray([config['TOSSIT']['TOSSIT_y'], config['TOSSIT']['TOSSIT_x']]).T
-    min_x = config['scaling']['min_x']
-    max_x = config['scaling']['max_x']
-    min_y = config['scaling']['min_y']
-    max_y = config['scaling']['max_y']
+#     # load constants
+#     TOSSIT_locations = np.asarray([config['TOSSIT']['TOSSIT_y'], config['TOSSIT']['TOSSIT_x']]).T
+#     min_x = config['scaling']['min_x']
+#     max_x = config['scaling']['max_x']
+#     min_y = config['scaling']['min_y']
+#     max_y = config['scaling']['max_y']
 
-    # define projection object
-    pargs = proj.Proj(proj="aeqd", lat_0=41.9108, lon_0=-70.4292, datum="WGS84", units="m")
+#     # define projection object
+#     pargs = proj.Proj(proj="aeqd", lat_0=41.9108, lon_0=-70.4292, datum="WGS84", units="m")
     
-    # get lon/lat bounds for the map
-    lon, lat = pargs([min_x-buffer, max_x+buffer], [min_y-buffer, max_y+buffer], inverse=True)
-    region = [*lon, *lat]
+#     # get lon/lat bounds for the map
+#     lon, lat = pargs([min_x-buffer, max_x+buffer], [min_y-buffer, max_y+buffer], inverse=True)
+#     region = [*lon, *lat]
 
-    # convert sensor locs to lat/lon
-    lon_TOSSIT, lat_TOSSIT = pargs(TOSSIT_locations[:,1], -TOSSIT_locations[:,0], inverse=True)
+#     # convert sensor locs to lat/lon
+#     lon_TOSSIT, lat_TOSSIT = pargs(TOSSIT_locations[:,1], -TOSSIT_locations[:,0], inverse=True)
 
-    fig = pygmt.Figure()
-    fig.basemap(region=region, projection="M15c", frame=True)
-    fig.coast(land="black", water="skyblue4")
-    fig.plot(x=lon_TOSSIT, y=lat_TOSSIT, style="t0.3c", fill="green", pen="black", label="sensors")
-    for i, l in enumerate(locs_est):
-        lon_est, lat_est = pargs(l[:,1], -l[:,0], inverse=True)
-        fig.plot(x=lon_est, y=lat_est, style="x0.3c", pen=f"1p,{colors[i][0]}/{colors[i][1]}/{colors[i][2]}", label=f'estimate ({dates[i]})' if dates[0] else 'estimate')
+#     fig = pygmt.Figure()
+#     fig.basemap(region=region, projection="M15c", frame=True)
+#     fig.coast(land="black", water="skyblue4")
+#     fig.plot(x=lon_TOSSIT, y=lat_TOSSIT, style="t0.3c", fill="green", pen="black", label="sensors")
+#     for i, l in enumerate(locs_est):
+#         lon_est, lat_est = pargs(l[:,1], -l[:,0], inverse=True)
+#         fig.plot(x=lon_est, y=lat_est, style="x0.3c", pen=f"1p,{colors[i][0]}/{colors[i][1]}/{colors[i][2]}", label=f'estimate ({dates[i]})' if dates[0] else 'estimate')
     
-    fig.legend(transparency=legend_transparency)
+#     fig.legend(transparency=legend_transparency)
 
-    if save is not None:
-        fig.savefig(save)
+#     if save is not None:
+#         fig.savefig(save)
 
 def has_len(x):
     """
@@ -258,7 +259,7 @@ def scan_experimental_data(model, start, end, chunk_size, overlap_fraction, orde
                                 row_dict["x"].append(np.around(locs_est[i,1], 2))
                                 row_dict["y"].append(np.around(locs_est[i,0], 2))
                             id += 1
-                        
+
                         new_row = pd.DataFrame(row_dict)
                         new_row.to_csv(csv_path, mode='a', index=False, header=False)
                     # update pointer and proress bar
@@ -330,6 +331,7 @@ if __name__ == '__main__':
     all_dates = []
     for _, dfg_bin in bin_grouped:
         id_grouped = dfg_bin.groupby(by="id")
+
         x, y = [],[]
         date = None 
         for _, dfg in id_grouped:
@@ -341,9 +343,9 @@ if __name__ == '__main__':
 
         # plot locations by day
         locs_est = np.stack([y, x], axis=1)
-        plot_localization(locs_est, title="CCB-2023 Location Estimates", save=os.path.join(fig_dir, f"locations_{date.date()}.png"), dates=date.date())
+        plotting.plot_localization(locs_est, title="CCB-2023 Location Estimates", save=os.path.join(fig_dir, f"locations_{date.date()}.png"), dates=date.date(), bins=5)
         all_dates.append(date.date())
         all_locs_est.append(locs_est)
         
     # plot all days
-    plot_localization(all_locs_est, title="CCB-2023 Location Estimates", save=os.path.join(fig_dir, f"locations_all_dates.png"), dates=all_dates, legend_transparency=70)
+    plotting.plot_localization(all_locs_est, title="CCB-2023 Location Estimates", save=os.path.join(fig_dir, f"locations_all_dates.png"), dates=all_dates, legend_transparency=70, bins=5)
--- whale_gunshot_localization/utils/plotting.py
+++ whale_gunshot_localization/utils/plotting.py
@@ -1,6 +1,9 @@
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+
 import numpy as np
 import pyproj as proj
-import pygmt
+# import pygmt
 
 from whale_gunshot_localization import config
 
@@ -33,7 +36,7 @@ def plot_localization(locs_est, buffer=6000, title=None, dates=None, save=None, 
 
     # random list of color for plotting
     rng = np.random.default_rng(1111)
-    colors = [rng.uniform(0, 255, size=3) for _ in range(len(locs_est))]
+    colors = [rng.uniform(0, 1, size=3) for _ in range(len(locs_est))]
 
     # load constants
     TOSSIT_locations = np.asarray([config['TOSSIT']['TOSSIT_y'], config['TOSSIT']['TOSSIT_x']]).T
@@ -42,30 +45,62 @@ def plot_localization(locs_est, buffer=6000, title=None, dates=None, save=None, 
     min_y = config['scaling']['min_y']
     max_y = config['scaling']['max_y']
 
-    # define projection object
+    # define projection objectalgorithm_sandbox
     pargs = proj.Proj(proj="aeqd", lat_0=41.9108, lon_0=-70.4292, datum="WGS84", units="m")
     
     # get lon/lat bounds for the map
-    lon, lat = pargs([min_x-buffer, max_x+buffer], [min_y-buffer, max_y+buffer], inverse=True)
-    region = [*lon, *lat]
+    # lon, lat = pargs([min_x-buffer, max_x+buffer], [min_y-buffer, max_y+buffer], inverse=True)
+    # region = [*lon, *lat]
 
     # convert sensor locs to lat/lon
     lon_TOSSIT, lat_TOSSIT = pargs(TOSSIT_locations[:,1], -TOSSIT_locations[:,0], inverse=True)
 
-    fig = pygmt.Figure()
-    fig.basemap(region=region, projection="Cyl_stere10c", frame=True)
-    fig.coast(land="black", water="skyblue4")
-    fig.plot(x=lon_TOSSIT, y=lat_TOSSIT, style="t0.3c", fill="green", pen="black", label="sensors")
+    # define basemap
+    width = abs(config['scaling']['max_x'] - config['scaling']['min_x']) + 2*buffer
+    height = abs(config['scaling']['max_y'] - config['scaling']['min_y']) + 2*buffer
+    m = Basemap(width=width, height=height, projection='aeqd',
+                lat_0=41.9108, lon_0=-70.4292, resolution="f")
+    lon, lat = pargs([-width / 2, width / 2], [-height / 2, height / 2], inverse=True)
+
+    # fill background.
+    m.drawmapboundary(fill_color='white')
+    # draw coasts and fill continents.
+    m.drawcoastlines(linewidth=0.5)
+    m.fillcontinents(color='white',lake_color='white')
+
+
+    m.plot(lon_TOSSIT, lat_TOSSIT, 'g^', markeredgecolor='black', latlon=True, label='sensors')
+    
     for i, l in enumerate(locs_est):
         lon_est, lat_est = pargs(l[:,1], -l[:,0], inverse=True)
         if bins:
-            H, x_edges, y_edges = np.histogram2d(lon, lat, bins=bins)
-            print(np.diff(x_edges), np.diff(y_edges))
-            fig.histogram(H, series=bins, fill=True)
-        else:
-            fig.plot(x=lon_est, y=lat_est, style="x0.3c", pen=f"1p,{colors[i][0]}/{colors[i][1]}/{colors[i][2]}", label=f'estimate ({dates[i]})' if dates[0] else 'estimate')
-    
-    fig.legend(transparency=legend_transparency)
+            H, x_edges, y_edges = np.histogram2d(lat_est, lon_est, bins=bins)
+            xx, yy = np.meshgrid(y_edges, x_edges)
+            colormesh = m.pcolormesh(xx, yy, H, latlon=True, cmap='Greens')
+        m.plot(lon_est, lat_est, 'x', color=(colors[i][0], colors[i][1], colors[i][2]), latlon=True, label=f'estimate ({dates[i]})' if dates[0] else 'estimate')
 
-    if save is not None:
-        fig.savefig(save)
+    cb = m.colorbar(colormesh, location='right', label="")
+
+    m.drawparallels(np.arange(np.floor(lat[0]), np.ceil(lat[1]), 0.2), labels=[1, 0, 0, 0], color="None")
+    m.drawmeridians(np.arange(np.floor(lon[0]), np.ceil(lon[1]), 0.1), labels=[0, 0, 0, 1], color="None")
+    plt.legend(loc='upper left')
+    plt.savefig(save)
+    plt.close()
+
+    # fig = pygmt.Figure()
+    # fig.basemap(region=region, projection="Cyl_stere10c", frame=True)
+    # fig.coast(land="black", water="skyblue4")
+    # fig.plot(x=lon_TOSSIT, y=lat_TOSSIT, style="t0.3c", fill="green", pen="black", label="sensors")
+    # for i, l in enumerate(locs_est):
+    #     lon_est, lat_est = pargs(l[:,1], -l[:,0], inverse=True)
+    #     if bins:
+    #         H, x_edges, y_edges = np.histogram2d(lon, lat, bins=bins)
+    #         print(np.diff(x_edges), np.diff(y_edges))
+    #         fig.histogram(H, series=bins, fill=True)
+    #     else:
+    #         fig.plot(x=lon_est, y=lat_est, style="x0.3c", pen=f"1p,{colors[i][0]}/{colors[i][1]}/{colors[i][2]}", label=f'estimate ({dates[i]})' if dates[0] else 'estimate')
+    
+    # fig.legend(transparency=legend_transparency)
+
+    # if save is not None:
+    #     fig.savefig(save)
