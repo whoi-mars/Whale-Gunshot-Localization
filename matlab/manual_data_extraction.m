68335270be9c6659b06828d64e197c056d887d88
Mark Goldwater <goldwater243@gmail.com>
2024-01-12 17:04:54 -0500

    Update

---
+++ matlab/manual_data_extraction.m
@@ -0,0 +1,13 @@
+%%
+
+clear all
+close all
+clc
+
+%%
+
+files = [];
+timestamps = [];
+
+% addpath('yaml')
+% config = yaml.loadFile("../whale_gunshot_localization/config_12.yaml");
--- scripts/scan_experimental_data.py
+++ scripts/scan_experimental_data.py
@@ -130,7 +130,7 @@ def scan_experimental_data(model, start, end, chunk_size, overlap_fraction, orde
     CA = experimental.ClipAnalyzer(model, data_params, preprocessor=experimental.l2_standardize, device=device)
 
     # prepare localizer object
-    l = experimental.Localizer(k=4, multilat=experimental.MultilaterationOpt(method_thresh=0.95), consistency_thresh=500, prune=False)
+    l = experimental.Localizer(k=4, multilat=experimental.MultilaterationOpt(method_thresh=float('inf')), consistency_thresh=500, prune=False)
 
     # get starts of chunks to read and total days
     chunk_starts = pd.date_range(start=start, end=end, freq=f"{chunk_size}s")
@@ -244,7 +244,7 @@ if __name__ == '__main__':
         model = TCNRangeAndClassify(input_size=232, 
                                     num_channels=7*[368],
                                     kernel_size=6,
-                                    dropout=0.05).to(device)
+                                    dropout=0.3).to(device)
 
         # load model weights
         state_dict = torch.load(os.path.join(config['models']['checkpoints_directories'], config['models']['model_dir'], 'weights_best.pt'),
@@ -253,48 +253,3 @@ if __name__ == '__main__':
 
         # perform scanning
         scan_experimental_data(model, args.start, args.end, 2 * args.half_window, args.overlap_fraction, args.ordered_sensors, args.background)
-
-    # ###################################
-    # #           plot results          #
-    # ###################################
-
-    # fig_dir = os.path.join(PROJECT_ROOT_DIR, "scripts", "results", config['models']['model_dir'], "detection_maps")
-    # Path(fig_dir).mkdir(exist_ok=True, parents=True)
-    
-    # csv_path = os.path.join(PROJECT_ROOT_DIR, "scripts", "results", config['models']['model_dir'], "multi_scan_results.csv")
-    # if not os.path.exists(csv_path):
-    #     raise IOError("no results file")
-    # df = pd.read_csv(csv_path)
-
-    # # get bin edges
-    # bins_dt = pd.date_range(start=pd.to_datetime(start_time_dict[wav_files[0]]).date(), end=(pd.to_datetime(end_time_dict[wav_files[-1]]) + pd.Timedelta(1, "d")).date(), freq="D")
-    # df["bin"] = pd.to_datetime(pd.cut(pd.DatetimeIndex(df["global_timestamp"]), bins=bins_dt, labels=bins_dt[:-1]))
-    # bin_grouped = df.groupby(by="bin")
-
-    # # # load map
-    # # Image.MAX_IMAGE_PIXELS = 729744000
-    # # bathym = Image.open(os.path.join(config['dataset']['data_directory'], "mikesbathym.tif"))
-
-    # # make plots
-    # all_locs_est = []
-    # all_dates = []
-    # for _, dfg_bin in bin_grouped:
-    #     id_grouped = dfg_bin.groupby(by="id")
-
-    #     x, y = [],[]
-    #     date = None 
-    #     for _, dfg in id_grouped:
-    #         dfg = dfg.reset_index()
-    #         x.append(dfg.loc[0,"x"])
-    #         y.append(dfg.loc[0,"y"])
-    #         if date is None:
-    #             date = dfg.loc[0,"bin"]
-
-    #     # plot locations by day
-    #     locs_est = np.stack([y, x], axis=1)
-    #     plotting.plot_localization(locs_est, title="CCB-2023 Location Estimates", save=os.path.join(fig_dir, f"locations_{date.date()}.png"), dates=date.date(), bins=10, d_lat=0.2, d_lon=0.1)
-    #     all_dates.append(date.date())
-    #     all_locs_est.append(locs_est)
-        
-    # # plot all days
-    # plotting.plot_localization(all_locs_est, title="CCB-2023 Location Estimates", save=os.path.join(fig_dir, f"locations_all_dates.png"), dates=all_dates, bins=10, d_lat=0.2, d_lon=0.1)
